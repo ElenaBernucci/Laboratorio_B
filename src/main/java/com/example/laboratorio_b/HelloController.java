d9dfb6dc84640b7682993aee6177adcfd36c56f7
elena <elena.bernucci00@gmail.com>
2022-04-20 15:57:46 +0200

    Initial commit

---
+++ src/main/java/com/example/laboratorio_b/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.laboratorio_b;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.laboratorio_b {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.laboratorio_b to javafx.fxml;
+    exports com.example.laboratorio_b;
+}
